37c2a52f591e14fc3ba000b90a38f07012ae6e3a
unknown <manju@praella.com>
2022-12-06 12:19:56 +0530

    first-commit

---
+++ Manju shopify/Custom-Cart-Drawer-Functionality/assets/recharge-dashboard.js
@@ -0,0 +1,11 @@
+const customSelectors = {};
+class dashboardJS {
+    constructor() {
+        customerDetails();
+    }
+}
+function customerDetails(){
+    
+}
+
+typeof dashboardJS !== 'undefined' && new dashboardJS();
